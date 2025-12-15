00c0880a1f0c0a311dc8dbc5d25be0ac551103e7
DesperadoA1ngel <sersamuremon1609@gmail.com>
2025-12-15 19:47:36 +0700

    Add files via upload
    переделал чтобы работало
    исправил ошибки с qt
    написал скрипты для билда и запуска, только хз зачем
    узнал что коммиты делать с vs 2022 почему то не получается
    
    нужно подправить сортировку по id, кажется что накосячил с типами данных

---
+++ phonebook/build/CMakeFiles/PhoneBook.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhoneBook_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/PhoneBook_autogen.dir/ParseCache.txt"
+  "PhoneBook_autogen"
+  "CMakeFiles/PhoneBook.dir/PhoneBook_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/src/contact.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/src/main.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/src/mainwindow.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/src/phonebook.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/src/searchdialog.cpp.obj"
+  "PhoneBook.exe"
+  "PhoneBook.exe.manifest"
+  "PhoneBook.pdb"
+  "libPhoneBook.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhoneBook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
